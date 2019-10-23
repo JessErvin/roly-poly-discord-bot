8647def15200e8d44046da05c3317784548a2e83
mmmSprinkles <jemail9@gmail.com>
2019-10-23 10:14:30 -0700

    Added the new !commands command, additional comments & cleaned up formatting.

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
+'''
+This is roly-poly, a simple dice rolling bot I made for Discord.
+It can also say hello or draw a card from the Deck of Many Things
+'''
+
 import discord
 import random
 import pandas as pd
 from discord.ext import commands
 import config
 
-TOKEN = config.TOKEN
+TOKEN = config.token
 
 client = commands.Bot(command_prefix='!')
 
@@ -19,59 +24,76 @@ async def on_ready():
 
 
 @client.event
-async def on_message(message):
+async def on_message(message):  # Say Hi to the bot!
     if message.content.startswith('!hello'):
         channel = message.channel
-        await channel.send('Hello, {.author.mention}'.format(message))
+        await channel.send('Hello, {.author.mention}!'.format(message))
 
-    if message.content.startswith('!deck13'):
+    if message.content.startswith('!deck13'):  # pick a card from the 13 card version of the deck of many things
         channel = message.channel
         card = random.randint(0, 13)
         df = pd.read_csv('deckofmanythings.csv')
         cardName = df['Playing Card'][card]
         cardDesc = df['Description'][card]
         await channel.send((
-                                       "{0.author.mention}, you pull a card from the Deck of Many Things...\nYou have selected the " + cardName + ":\nThe Card of the " + cardDesc).format(
+                "{0.author.mention}, you pull a card from the Deck of Many Things...\nYou have "
+                "selected the " + cardName + ":\nThe Card of the " + cardDesc).format(
             message))
 
-    if message.content.startswith('!deck22'):
+    if message.content.startswith('!deck22'):  # pick a card from the 22 card version of the deck of many things
         channel = message.channel
         card = random.randint(0, 22)
         df = pd.read_csv('deckofmanythings.csv')
         cardName = df['Playing Card'][card]
         cardDesc = df['Description'][card]
         await channel.send((
-                                       "{0.author.mention}, you pull a card from the Deck of Many Things...\nYou have selected the " + cardName + ":\nThe Card of the " + cardDesc).format(
+                '{0.author.mention}, you pull a card from the Deck of Many Things...\nYou have selected the ' + cardName + ':\nThe Card of the ' + cardDesc).format(
             message))
 
-    if message.content.startswith('!roll'):
+    if message.content.startswith('!roll'):  # roll some dice & add modifiers
         channel = message.channel
         roll = str(message.content).split(' ')[1]
         numDice = int(roll.split('d')[0])
         diceVal = int(roll.split('d')[-1])
         diceResults = []
         total = 0
-        if numDice > 100:
-            await channel.send(
-                ('{0.author.mention} That is too many dice!').format(message))
-        elif diceVal > 1000:
-            await channel.send(
-                ('{0.author.mention} That die is too big!').format(message))
-        else:
-            while numDice > 0:
-                diceResults.append(d(diceVal))
-                numDice -= 1
-            for x in diceResults:
-                total += x
-            if '+' in message.content:
-                total += int(message.content[-1])
-            elif '-' in message.content:
-                total -= int(message.content[-1])
+        try:
+            if numDice > 100:  # setting limits to avoid timing out
+                await channel.send(
+                    '{0.author.mention} That is too many dice!'.format(message))
+            elif diceVal > 1000:
+                await channel.send(
+                    '{0.author.mention} That die is too big!'.format(message))
             else:
-                total += 0
+                while numDice > 0:
+                    diceResults.append(d(diceVal))
+                    numDice -= 1
+                for x in diceResults:
+                    total += x
+                if '+' in message.content:
+                    total += int(message.content[-1])
+                elif '-' in message.content:
+                    total -= int(message.content[-1])
+                else:
+                    total += 0
 
+                await channel.send(
+                    ('{0.author.mention} Result: ' + str(total) + '\n' + '*' + str(diceResults) + '*').format(message))
+        except:
             await channel.send(
-                ('{0.author.mention} Result: ' + str(total) + '\n' + '*' + str(diceResults) + '*').format(message))
+                "I'm sorry, {0.author.mention} I don't understand, Try typing !commands to see what I can do".format(
+                    message))
+
+    if message.content.startswith('!commands'):  # returns a list of possible commands
+        channel = message.channel
+        await channel.send((
+            'Hello, {0.author.mention}! Here is a list of what I can do:\n__**!roll #d# + #**__ - This rolls your '
+            'chosen number of dice with your chosen number of sides. You can also add or subtract a modifier! '
+            '\n     *Examples: !roll 1d20, !roll 2d6 + 8, !roll 2d4 - 1*\n__**!deck13**__ - This will pull a card from '
+            'the 13 card version of the Deck of Many Things\n__**!deck22**__ - This will pull a card from the full 22 '
+            'card version of the Deck of Many Things\n__**!commands**__ - This will repeat this message!\n     *Details '
+            'on the Deck of Many Things can be found here: '
+            'https://www.dndbeyond.com/magic-items/deck-of-many-things*'.format(message)))
 
 
 client.run(TOKEN)
